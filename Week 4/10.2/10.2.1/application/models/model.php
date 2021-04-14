f0c372a075aaaaa2470aacc78a154f494d27d22d
violetflower13 <68681793+chienit3bk@users.noreply.github.com>
2021-04-14 16:44:26 +0700

    Chien commit

---
+++ Week 4/10.2/10.2.1/application/models/model.php
@@ -0,0 +1,13 @@
+<?php
+    class Model extends SQLQuery {
+        protected $_model;
+
+        function __construct() {
+            $this->connect(DB_HOSt, DB_USER, DB_PASSOWRD, DB_NAME);
+            $this->_model = get_class($this);
+            $this->_table = strtolower($this->_model)."s";
+
+        }
+        function __destruct() {
+        }
+    }
