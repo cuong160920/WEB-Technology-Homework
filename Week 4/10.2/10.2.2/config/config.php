6f92681fdcbc1cc48876ad73adab10a4e44053d2
datbt183880 <dat.bt183880@sis.hust.edu.vn>
2021-04-15 01:52:21 +0700

    Dat commit

---
+++ Week 4/10.2/10.2.2/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+/** Configuration Variables **/
+
+define ('DEVELOPMENT_ENVIRONMENT',true);
+
+
+define('DB_NAME', 'yourdatabasename');
+define('DB_USER', 'yourusername');
+define('DB_PASSWORD', 'yourpassword');
+define('DB_HOST', 'localhost');
+
+define('BASE_PATH','http://localhost/framework');
+
+
+define('PAGINATE_LIMIT', '5');
